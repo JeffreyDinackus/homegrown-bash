e58c0f2cec00d810d49d7ad9244a5f748b16a4b5
JeffreyDinackus <z3l9m@protonmail.com>
2023-02-03 18:29:46 -0500

    adding new del1000users script, run this one if you run the make1000users.sh script and want to reverse

---
+++ fun/del1000users.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#this script reverses the make1000users.sh script
+#this script will delete any user whose name is 1 to 1000 so be careful
+
+echo "it'll be okay if you just enter your password..."
+
+su -
+
+x=1
+
+for x in {1..1000}
+do
+    sudo userdel "$x"
+    echo "I deleted user #$x"
+done
