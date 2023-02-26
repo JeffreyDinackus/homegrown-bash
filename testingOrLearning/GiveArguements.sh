d102d5db769405a9f7d263c44be3b294628cd814
JeffreyDinackus <z3l9m@protonmail.com>
2023-02-26 16:33:17 -0500

    new scripts to learn how to handle varaibles

---
+++ testingOrLearning/GiveArguements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "$1 is the first arguement"
+echo "$2 is the second arguement"
+echo "$3 is the third arguement"
+echo "$1 is the first arguement"
---
+++ testingOrLearning/hardcoded.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -- Comptia Dion Training
+
+echo "$1 is the first arguement"
+echo "$2 is the second arguement"
+echo "$3 is the third arguement"
+echo "$1 is the first arguement"
