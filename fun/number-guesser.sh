0336a5247b611894398412707a2b7ed9dc100d4f
JeffreyDinackus <z3l9m@protonmail.com>
2023-03-02 20:20:29 -0500

    the number guesser game has more functionality

--- fun/number-guesser.sh
+++ fun/number-guesser.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 
+count=`cat num-guess-count.txt`
+
+
 echo "Guess a number if you want to play"
 read guess
 echo "You guessed $number"
+echo "You have guessed $count times so far"
 
 echo "the correct number is:"
 echo $(( $RANDOM % 1000	 + 1 ))
+
+let "count++"
+
+echo $count > num-guess-count.txt
+
